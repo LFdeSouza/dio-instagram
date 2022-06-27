8f3d8a250564191228e49b1effac2412e7b3c7b4
Luiz Felipe de Souza <luiz.f.souza@live.com>
2022-06-27 15:01:00 -0300

    layout corrections and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "custom-break": "835px",
+      },
+    },
   },
   plugins: [],
 };
